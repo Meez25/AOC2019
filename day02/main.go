21cbfde05578f4647796a056a15d1121579ca30e
Yann BRONNER <yann.bronner@gmail.com>
2025-01-17 10:10:23 +0100

    day 2 p2

--- day02/main.go
+++ day02/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	_ "embed"
+	"errors"
 	"fmt"
 	"time"
 )
@@ -14,30 +15,21 @@ func main() {
 	start := time.Now()
 	table := bytes.Split(bytes.TrimSpace(inputFile), []byte(","))
 	asInt := make([]int, len(table))
+
 	for i := range table {
 		asInt[i] = convertByteToInt(table[i])
 	}
 
-	i := 0
-	asInt[1] = 12
-	asInt[2] = 2
-	for {
-		if i >= len(asInt) {
-			break
-		}
-		if asInt[i] == 99 {
-			break
-		}
-		if asInt[i] == 1 {
-			asInt[asInt[i+3]] = asInt[asInt[i+1]] + asInt[asInt[i+2]]
-		}
-		if asInt[i] == 2 {
-			asInt[asInt[i+3]] = asInt[asInt[i+1]] * asInt[asInt[i+2]]
-		}
-		i += 4
-	}
+	initialMem := make([]int, len(asInt))
+	copy(initialMem, asInt)
+
+	runProgram(asInt)
+	copy(asInt, initialMem)
+
 	elapsed := time.Since(start)
 	fmt.Println("Part 1 : ", asInt[0], "Done in ", elapsed)
+	fmt.Println("Part 2 : ", solveP2(initialMem), "Done in ", time.Since(start))
+
 }
 
 func convertByteToInt(input []byte) int {
@@ -46,4 +38,73 @@ func convertByteToInt(input []byte) int {
 		num = num*10 + int(b-'0')
 	}
 	return num
+}
+
+func solve1(memory []int) (int, error) {
+	i := 0
+	memory[1] = 12
+	memory[2] = 2
+	for {
+		if i >= len(memory) || memory[i] == 99 {
+			break
+		}
+
+		if memory[i+1] >= len(memory) || memory[i+2] >= len(memory) || memory[i+3] >= len(memory) {
+			return 0, errors.New("Memory error")
+		}
+
+		if memory[i] == 1 {
+			memory[memory[i+3]] = memory[memory[i+1]] + memory[memory[i+2]]
+		}
+
+		if memory[i] == 2 {
+			memory[memory[i+3]] = memory[memory[i+1]] * memory[memory[i+2]]
+		}
+		i += 4
+	}
+	return memory[0], nil
+}
+
+func runProgram(memory []int) error {
+	i := 0
+	for {
+		if i >= len(memory) || memory[i] == 99 {
+			break
+		}
+
+		if memory[i+1] >= len(memory) || memory[i+2] >= len(memory) || memory[i+3] >= len(memory) {
+			return errors.New("Memory error")
+		}
+
+		if memory[i] == 1 {
+			memory[memory[i+3]] = memory[memory[i+1]] + memory[memory[i+2]]
+		}
+
+		if memory[i] == 2 {
+			memory[memory[i+3]] = memory[memory[i+1]] * memory[memory[i+2]]
+		}
+		i += 4
+	}
+	return nil
+}
+
+func solveP2(memory []int) int {
+	initialMem := make([]int, len(memory))
+	copy(initialMem, memory)
+
+	for i := 0; i < 100; i++ {
+		for y := 0; y < 100; y++ {
+			copy(memory, initialMem)
+			memory[1] = y
+			memory[2] = i
+			err := runProgram(memory)
+			if err != nil {
+				continue
+			}
+			if memory[0] == 19690720 {
+				return y*100 + i
+			}
+		}
+	}
+	return 0
 }
